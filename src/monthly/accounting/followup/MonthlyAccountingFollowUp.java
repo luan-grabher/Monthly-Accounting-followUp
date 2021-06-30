11979d66a97ece6f4f0a98be1f9032a2aa21a0ff
User <User@DESKTOP-D7T61HD>
2021-06-30 09:55:16 -0300

    start commit

--- src/monthly/accounting/followup/MonthlyAccountingFollowUp.java
+++ src/monthly/accounting/followup/MonthlyAccountingFollowUp.java
@@ -1,21 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package monthly.accounting.followup;
 
-/**
- *
- * @author User
- */
-public class MonthlyAccountingFollowUp {
+import java.util.Calendar;
+import javax.swing.JOptionPane;
 
-    /**
-     * @param args the command line arguments
-     */
+public class MonthlyAccountingFollowUp {
+    public static Integer year = 2021;
+    public static Integer enterprise = 0;
+
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            //Pega Empresa
+            enterprise = Integer.valueOf(JOptionPane.showInputDialog("Qual o NÚMERO/CÓDIGO da empresa no ÚNICO?").replaceAll("[^0-9]", "0"));
+            
+            if(enterprise > 0){            
+                //Pega Ano
+                Integer nowYear = Calendar.getInstance().get(Calendar.YEAR);                
+                year = (Integer) JOptionPane.showInputDialog(null, "Qual o ano da Pesquisa?", "Escolha o ano", JOptionPane.QUESTION_MESSAGE, null, new Integer[]{nowYear,nowYear-1,nowYear-2}, 0);
+
+                System.out.println("Opção escolhida: " + year);
+            }else{
+                throw new Error("Empresa inválida!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e, "Ocorreu um ERRO", JOptionPane.ERROR_MESSAGE);
+        } catch (Error e){
+            JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage(), "Ocorreu um ERRO", JOptionPane.ERROR_MESSAGE);
+        }
     }
     
 }

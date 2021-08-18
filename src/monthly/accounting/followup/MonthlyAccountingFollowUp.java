b1d776b7de94457d60a381c3fbe940259046a8b7
User <User@DESKTOP-D7T61HD>
2021-08-18 12:28:54 -0300

    alterado os tempos de jogo

--- src/monthly/accounting/followup/MonthlyAccountingFollowUp.java
+++ src/monthly/accounting/followup/MonthlyAccountingFollowUp.java
@@ -6,10 +6,37 @@ import javax.swing.JOptionPane;
 public class MonthlyAccountingFollowUp {
     public static Integer year = 2021;
     public static Integer enterprise = 0;
+    
+    /**
+     * MODELO DE MAPA DE CONTA
+     * "338" = {
+            "2020" = {
+
+            },
+            "2021" = {
+                    1 = {
+                            "debito" = 0,
+                            "credito" = 0,
+                            "diferenca" = 0
+                    },
+                    2 = {
+                            "debito" = 0,
+                            "credito" = 0,
+                            "diferenca" = 0
+                    },
+                    3 = {
+                            "debito" = 0,
+                            "credito" = 0,
+                            "diferenca" = 0
+                    }
+            }
+        }
+     */
 
     
     public static void main(String[] args) {
-        try {
+        try {           
+            
             //Pega Empresa
             enterprise = Integer.valueOf(JOptionPane.showInputDialog("Qual o NÚMERO/CÓDIGO da empresa no ÚNICO?").replaceAll("[^0-9]", "0"));
             
@@ -21,7 +48,28 @@ public class MonthlyAccountingFollowUp {
                 System.out.println("Opção escolhida: " + year);
                 /**
                  * Busca no banco:
-                 * -- Total de débito de cada mês 
+                 * -- Total de débito de cada mês
+                 * -- Total de credito de cada mês
+                 * 
+                 * Percorre todos totais de debito
+                 * -- Se não tiver a conta no mapa de contas
+                 * ---- Adiciona um mapa com os anos e meses para aquela conta
+                 * -- Adiciona no mapa da conta naquele ano e mes o valor
+                 * 
+                 * Percorre todos totais de credito
+                 * -- Se não tiver a conta no mapa de contas
+                 * ---- Adiciona um mapa com os anos e meses para aquela conta
+                 * -- Adiciona no mapa da conta naquele ano e mes o valor
+                 * -- Faz a diferença entre debito e credito
+                 * 
+                 * Abre o arquivo Excel
+                 * Troca a celula "ano" para o ano utilizado
+                 * Percorre todos mapas de contas
+                 * -- Vai de 1 a 12
+                 * ---- Coloca os valores de diferenca na coluna do mês (Será feito um calculo para descrobrir qual a coluna é de cada mes)
+                 * Salva Excel
+                 * 
+                 * 
                  * Busca no banco de dados o saldo das contas no dia 31/12 do ano informado e do ano anterior
                  */
             }else{

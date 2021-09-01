5b27c519f188da343d70fbf6fb7704a589ab9297
User <User@DESKTOP-D7T61HD>
2021-09-01 14:03:52 -0300

    start excel file class

--- src/Model/ExcelFile.java
+++ src/Model/ExcelFile.java
@@ -1,10 +1,27 @@
 
 package Model;
 
+import JExcel.JExcel;
 import fileManager.FileManager;
 import java.io.File;
 
 
 public class ExcelFile {
     private static File file = FileManager.getFile("Acompanhamento_Mensal_Template.xslx");
+        
+    public static boolean createExcelFile(){
+        try {
+            //Abrir template
+            XSSFWorkbook wk = new XSSFWorkbook(file);           
+
+            colarIdConfigs(wk.getSheet("Parâmetros Gerais"));
+            transferirLctos(wk.getSheet("Dados"));
+
+            //salvar
+            //wk.close();
+            return JExcel.saveWorkbookAs(System.getProperty("user.profile") + "/desktop/" + "Acompanhamento_mensal.xlsx", wk);
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
 }

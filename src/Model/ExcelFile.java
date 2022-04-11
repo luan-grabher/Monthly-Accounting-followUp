0a9c9304f4be5775779084ff1867885ad09ad75d
User <User@DESKTOP-D7T61HD>
2022-04-11 10:56:21 -0300

    remove netbeans project.

--- src/Model/ExcelFile.java
+++ src/Model/ExcelFile.java
@@ -5,15 +5,20 @@ import JExcel.Sheet;
 import static Model.Data.totais;
 import fileManager.FileManager;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.math.BigDecimal;
+import java.nio.file.Files;
 import java.util.HashMap;
 import static monthly.accounting.followup.MonthlyAccountingFollowUp.*;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 
 public class ExcelFile {
 
-    private static File file = FileManager.getFile("Acompanhamento_Mensal_Template.xlsx");
+    private static File templateFile = FileManager.getFile("Acompanhamento_Mensal_Template.xlsx");
 
     /*
                  * Percorre todos mapas de contas
@@ -26,10 +31,23 @@ public class ExcelFile {
      */
     public static boolean createExcelFile() {
         try {
-            //Abrir template
-            XSSFWorkbook wk = new XSSFWorkbook(file);
-            Sheet s = new Sheet(wk.getSheetAt(0));
+            //get desktop path
+            String desktopPath = System.getProperty("user.home") + File.separator + "Desktop";
 
+            //Set name of new file replacing 'Template' in the name of the template file with the year and enterprise
+            String newFileName = templateFile.getName().replace("Template", year + "-" + enterprise);
+
+            //Create new file
+            File newFile = new File(desktopPath + File.separator + newFileName);
+
+            //Copy template file to new file
+            Files.copy(templateFile.toPath(), newFile.toPath());
+
+            //Abrir new file
+            Workbook wk = WorkbookFactory.create(newFile);
+            Sheet s = new Sheet((XSSFSheet) wk.getSheetAt(0));
+
+            //Define o ano
             s.getRange("A8").setCellValue(year);
 
             Integer[] line = new Integer[]{1};
@@ -53,10 +71,16 @@ public class ExcelFile {
                 line[0]++;
             });
             
+            //Calcula diferenças
             XSSFFormulaEvaluator.evaluateAllFormulaCells(wk);
 
-            //Salva na area de trabalho
-            return JExcel.saveWorkbookAs(new File(System.getProperty("user.home") + "/desktop/" + "Acompanhamento_Mensal_" + enterprise + "__" + year + ".xlsx"), wk);
+            //Convert newFile to FileOutputStream
+            FileOutputStream fos = new FileOutputStream(newFile);
+            //Save newFile
+            wk.write(fos);
+
+            //Return true
+            return true;            
         } catch (Exception e) {
             throw new Error(e);
         }
